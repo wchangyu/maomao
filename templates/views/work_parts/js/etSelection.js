1994193bbeb14739d49f729495c63529e543da01
went <fenixwent@hotmail.com>
2016-09-18 11:03:51 +0800

    能耗分类选择器

---
+++ templates/views/work_parts/js/etSelection.js
@@ -0,0 +1,13 @@
+/**
+ * Created by went on 2016/9/18.
+ * 能耗分项类型的选择器
+ */
+
+var ETSelection = function(){
+    //设置楼宇相关的能耗种类
+    //$divETs
+    this.initPointers = function($divETs){
+
+    }
+}
+
