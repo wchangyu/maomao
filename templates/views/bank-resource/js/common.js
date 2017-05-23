a2cf18c9af3136d4eb4b1cbfd3ee6a695831a8a7
Your Name <15735291172@163.com>
2017-05-23 18:33:55 +0800

    1

--- templates/views/bank-resource/js/common.js
+++ templates/views/bank-resource/js/common.js
@@ -1,6 +1,76 @@
 /**
  * Created by admin on 2017/5/23.
  */
+
+var _userIdName = sessionStorage.getItem('userName');
+var IP1 = "http://192.168.1.109/BEEWebAPI/api";
+var IP2 = 'http://211.100.28.180/DingEAPI/api';
+var IP = IP1;
+
+var theTimes = 30000;
+
+//弹窗关闭时清空已输入过的信息
+
+$('.modal-header .close').on('click',function(){
+        $(this).parent().parent().parent().find('input').val('');
+});
+$('.modal-footer .btn-default').on('click',function(){
+
+        $(this).parent().parent().parent().find('input').val('');
+
+});
+
+//深拷贝的方法
+function deepCopy(src,obj){
+
+        obj = obj || (Array.isArray(src) ? [] : {});
+        for(var i in src){
+                if(src.hasOwnProperty(i)){
+                        if(typeof src[i] == 'object' && src[i]!=null){
+                                obj[i] = Array.isArray(src[i]) ? [] : {};
+                                deepCopy(src[i],obj[i]);
+                        }else{
+                                obj[i] = src[i];
+                        }
+                }
+        }
+}
+
+//自定义弹窗
+function myAlter(string){
+        $('#my-alert').modal('show');
+        $('#my-alert p b').html(string);
+}
+
+function CompareDate(d1,d2)
+{
+        return ((new Date(d1.replace(/-/g,"\/"))) > (new Date(d2.replace(/-/g,"\/"))));
+}
+
+
+
+function getFocus1(dom){
+        $('#my-alert').one('click','.btn-primary',function(){
+                dom.focus();
+        });
+}
+
+//日期控件
+$('.chooseDate').on('focus',function(){
+        var that = $(this);
+        setTimeout(function(){
+                $('.day').one('click',function(){
+                        console.log('ok');
+                        that.blur();
+                        $('.datepicker').css({
+                                display:'none'
+                        })
+
+                });
+        },100)
+
+});
+
 $('.chooseDate-month').datepicker(
     {
         startView: 1,
@@ -11,6 +81,9 @@ $('.chooseDate-month').datepicker(
         format: 'yyyy-mm'
     }
 );
+$('.chooseDate').on('focus',function(){
+        console.log('11');
+})
 //选择日期插件
 $('.chooseDate').datepicker(
     {
@@ -20,3 +93,87 @@ $('.chooseDate').datepicker(
         format: 'yyyy-mm-dd'
     }
 );
+
+//检验是否必填项全部填写
+function checkedNull(dom){
+        var checkNum = $(dom).find('.input-label').length;
+
+        for(var i=0; i< checkNum; i++){
+                if( $(dom).find('.input-label').eq(i).next().find('input').val() == ''){
+                        var txt = $(dom).find('.input-label').eq(i).next().find('input').parent().prev().html().split('：')[0];
+
+                        console.log(txt);
+                        myAlter(txt + " 不能为空")
+                        getFocus1($(dom).find('.input-label').eq(i).next().find('input'));
+                        return false;
+                };
+                if($(dom).find('.input-label').eq(i).next().find('.add-input-select').find('span').html() == ''){
+                        var txt = $(dom).children('.input-label').eq(i).html().split('：')[0];
+                        $('#check-text').modal('show');
+                        myAlter(txt + " 不能为空")
+                        return false;
+                };
+                if( $(dom).find('.input-label').eq(i).next().find('textarea').val() == ''){
+                        var txt = $(dom).find('.input-label').eq(i).next().find('textarea').parent().prev().html().split('：')[0];
+
+                        console.log(txt);
+                        myAlter(txt + " 不能为空")
+                        getFocus1($(dom).find('.input-label').eq(i).next().find('textarea'));
+                        return false;
+                };
+        }
+        return true;
+}
+
+//获取当前年月日
+function getNewDate(){
+
+        var mydate = new Date();
+        var str = "" + mydate.getFullYear() + "-";
+        str += (mydate.getMonth()+1) + "-";
+        str += mydate.getDate() + "";
+        return str;
+}
+//获取某个日期
+function getDate(mydate){
+        var str = "" + mydate.getFullYear() + "-";
+        str += (mydate.getMonth()+1) + "-";
+        str += mydate.getDate() + "";
+        return str;
+}
+
+//获取上个月开始结束日期
+function getLastMonth(){
+        var now = new Date();
+        var year = now.getFullYear();
+        var month = now.getMonth() + 1;
+
+
+        if(month == 1){
+                year = parseInt(year) - 1;
+                startDate = year +'-' + 12 + '-' + '1';
+                endDate = year +'-' + 12 + '-' + '31';
+        }else if(month == 3){
+
+                month = parseInt(month) - 1;
+                startDate = year +'-' + month + '-' + '1';
+                if(year%4==0 && year%100!=0 || year%400==0){
+
+                        endDate = year +'-' + month + '-' + '29';
+                }else{
+                        endDate = year +'-' + month + '-' + '28';
+                }
+        }else if(month == 2 || month == 4 || month == 6 || month == 9 || month == 11 || month == 8){
+                month = parseInt(month) - 1;
+                startDate = year +'-' + month + '-' + '1';
+
+                endDate = year +'-' + month + '-' + '31';
+
+        }else{
+                month = parseInt(month) - 1;
+                startDate = year +'-' + month + '-' + '1';
+
+                endDate = year +'-' + month + '-' + '30';
+
+        }
+}
--- templates/views/bank-resource/js/nenghaochaxun/page1.js
+++ templates/views/bank-resource/js/nenghaochaxun/page1.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     var myChart = echarts.init(document.getElementById('energy-demand'));
 
     // 指定图表的配置项和数据
-    var option = {
+    option = {
         title : {
             text: '未来一周气温变化',
             subtext: '纯属虚构'
@@ -50,7 +50,6 @@ $(document).ready(function() {
             {
                 name:'最高气温',
                 type:'line',
-                smooth:true,
                 data:[11, 11, 15, 13, 12, 13, 10],
                 itemStyle : {
                     normal : {
@@ -61,7 +60,7 @@ $(document).ready(function() {
                         }
                     }
                 },
-
+                smooth:true,
                 markPoint : {
                     data : [
                         {type : 'max', name: '最大值'},
@@ -77,17 +76,17 @@ $(document).ready(function() {
             {
                 name:'最低气温',
                 type:'line',
-                smooth:true,
                 data:[1, -2, 2, 5, 3, 2, 0],
                 itemStyle : {
                     normal : {
-                        color:'#f07bc5',
+                        color:'pink',
                         lineStyle:{
-                            color:'#f07bc5',
+                            color:'pink',
                             width:3
                         }
                     }
                 },
+                smooth:true,
                 markPoint : {
                     data : [
                         {name : '周最低', value : -2, xAxis: 1, yAxis: -1.5}
@@ -103,6 +102,7 @@ $(document).ready(function() {
     };
 
 
+
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
 
@@ -111,6 +111,243 @@ $(document).ready(function() {
             myChart.resize();
 
         }
+    };
+
+    //获取chart图中的数据
+    function getMainData(){
+
+        var energyItemID = $('#energy-type').val();
+
+        var objID = $('#obj-type').val();
+
+        var postArr = [];
+
+        var dateSign = '';
+
+        var startDate;
+
+        var endDate;
+
+        $(pointArr).each(function(i,o){
+
+            if(objID == o.enterpriseID){
+                postArr = o.pointerIDs;
+
+                return false;
+            }
+
+        });
+
+
+
+        var postDate = $('#post-date').val();
+
+        if(postDate == '今天'){
+
+            dateSign = '小时';
+
+            startDate = getNewDate();
+
+            console.log(startDate);
+
+            var now = new Date();
+
+            var tomorrow = new Date(now.setDate(now.getDate()+1));
+
+            endDate = getDate(tomorrow);
+
+        }else if(postDate == '昨天'){
+
+            dateSign = '小时';
+
+            endDate = getNewDate();
+
+
+            var now = new Date();
+
+            var yesterday = new Date(now.setDate(now.getDate()-1));
+
+            startDate = getDate(yesterday);
+
+            console.log(startDate);
+
+        }else if(postDate == '过去7天'){
+
+            dateSign = '日';
+
+            endDate = getNewDate();
+
+
+            var now = new Date();
+
+            var yesterday = new Date(now.setDate(now.getDate()-7));
+
+            startDate = getDate(yesterday);
+
+            console.log(startDate);
+        }else if(postDate == '过去30天'){
+
+            dateSign = '日';
+
+            endDate = getNewDate();
+
+
+            var now = new Date();
+
+            var yesterday = new Date(now.setDate(now.getDate()-30));
+
+            startDate = getDate(yesterday);
+
+            console.log(startDate);
+        }else if(postDate == '本月'){
+
+            dateSign = '日';
+
+            endDate = getNewDate();
+
+
+            var now = new Date();
+            var year = now.getFullYear();
+            var month = now.getMonth() + 1;
+
+            startDate = year + '-' + month + '-' + '1';
+
+            console.log(startDate);
+        }else if(postDate == '上月'){
+
+            dateSign = '日';
+
+            startDate = moment().subtract(1, 'month').startOf('month').format('YYYY-MM-DD');
+            endDate = moment().subtract(1, 'month').endOf('month').format('YYYY-MM-DD');
+
+            console.log(startDate,endDate);
+        }else if(postDate == '自定义'){
+
+            dateSign = '日';
+
+            startDate = $('.show-date').val().split('——')[0];
+            endDate = $('.show-date').val().split('——')[1];
+
+            console.log(startDate,endDate);
+        }
+
+        console.log(postArr);
+        $.ajax({
+            type: 'post',
+            url: IP + "/EnergyQuery/GetEnergyQueryTotalData",
+            timeout: theTimes,
+            data:{
+                "energyItemID":energyItemID,
+                "dateType": dateSign,
+                "startTime": startDate,
+                "endTime": endDate,
+                "pointerIDs":postArr
+            },
+            beforeSend: function () {
+
+            },
+
+            complete: function () {
+
+            },
+            success: function (data) {
+                $('#theLoading').modal('hide');
+                console.log(data);
+
+                var dataArr = data.ecMetaDatas;
+
+                var xArr = [];
+                var sArr = [];
+
+                $(dataArr).each(function(i,o){
+
+                    xArr.push(o.dataDate.split('T')[0]);
+                    sArr.push(o.data);
+                });
+
+                option = {
+                    title : {
+                        text: '未来一周气温变化',
+                        subtext: '纯属虚构'
+                    },
+                    tooltip : {
+                        trigger: 'axis'
+                    },
+                    legend: {
+                        data:['最高','最低']
+                    },
+                    toolbox: {
+                        show : true,
+                        feature : {
+                            mark : {show: true},
+                            dataView : {show: true, readOnly: false},
+                            magicType : {show: true, type: ['line', 'bar']},
+                            restore : {show: true},
+                            saveAsImage : {show: true}
+                        }
+                    },
+                    calculable : true,
+                    xAxis : [
+                        {
+                            type : 'category',
+                            boundaryGap : false,
+                            data :xArr
+                        }
+                    ],
+                    yAxis : [
+                        {
+                            type : 'value',
+                            axisLabel : {
+                                formatter: '{value} °C'
+                            }
+                        }
+                    ],
+                    series : [
+                        {
+                            name:'最高气温',
+                            type:'line',
+                            smooth:true,
+                            data:[11, 11, 15, 13, 12, 13, 10],
+                            itemStyle : {
+                                normal : {
+                                    color:'#53f4db',
+                                    lineStyle:{
+                                        color:'#53f4db',
+                                        width:3
+                                    }
+                                }
+                            },
+
+                            markPoint : {
+                                data : [
+                                    {type : 'max', name: '最大值'},
+                                    {type : 'min', name: '最小值'}
+                                ]
+                            },
+                            markLine : {
+                                data : [
+                                    {type : 'average', name: '平均值'}
+                                ]
+                            }
+                        }
+                    ]
+                };
+
+                myChart.setOption(option);
+
+
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                $('#theLoading').modal('hide');
+                console.log(textStatus);
+
+                if (textStatus == 'timeout') {//超时,status还有success,error等值的情况
+                    ajaxTimeoutTest.abort();
+                    myAlter("超时");
+                }
+                myAlter("请求失败！");
+            }
+        });
     };
 
 
@@ -123,24 +360,26 @@ $(document).ready(function() {
         var time = $('.condition-query li').eq(1).find('select').val();
         var theArea = $('.condition-query li').eq(2).find('select').val();
 
-        $('.show-title1').html(theArea);
-        $('.show-title2').html(type);
-        $('.show-title3').html(time);
+        //$('.show-title1').html(theArea);
+        //$('.show-title2').html(type);
+        //$('.show-title3').html(time);
 
-        $('#choose-date').modal('show');
+        getMainData();
 
     })
 
-});
-//获取页面初始数据
-function getStartData(){
 
+
+});
+
+var pointArr = [];
+//获取能耗查询页面初始数据
+function getStartData(){
+    //获取查询类别
     $.ajax({
         type: 'get',
-        url: IP + "/UnitMeter/IsRelevanceData",
-        async: false,
+        url: IP + "/EnergyQuery/GetShowEneryItemByQuery",
         timeout: theTimes,
-        data:id,
         beforeSend: function () {
 
         },
@@ -151,8 +390,12 @@ function getStartData(){
         success: function (data) {
             $('#theLoading').modal('hide');
             console.log(data);
-            postData = data;
-            console.log(postData);
+            var html= '';
+            for(var i=0; i<data.length;i++){
+                html +=   '<option value="'+data[i].f_EnergyItemID+'">'+data[i].f_EnergyItemName+'</option>'
+            }
+
+            $('#energy-type').html(html);
 
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
@@ -167,10 +410,117 @@ function getStartData(){
         }
     });
 
+    //获取查询对象
+
+    $.ajax({
+        type: 'get',
+        url: IP + "/EnergyQuery/GetEnterpriseItem",
+        timeout: theTimes,
+        data:{
+            userID :_userIdName,
+            isShowTotalItem : 1
+        },
+        beforeSend: function () {
+
+        },
+
+        complete: function () {
+
+        },
+        success: function (data) {
+            $('#theLoading').modal('hide');
+            console.log(data);
+            pointArr = data;
+            var html= '';
+            for(var i=0; i<data.length;i++){
+                html +=   '<option value="'+data[i].enterpriseID+'">'+data[i].eprName+'</option>'
+            }
+
+            $('#obj-type').html(html);
+
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            $('#theLoading').modal('hide');
+            console.log(textStatus);
+
+            if (textStatus == 'timeout') {//超时,status还有success,error等值的情况
+                ajaxTimeoutTest.abort();
+                myAlter("超时");
+            }
+            myAlter("请求失败！");
+        }
+    });
 }
+getStartData();
 
 //自定义时间时
-$('.custom-time').on('click',function(){
+$('.datatimeblock').on('change',function(){
+
+    $('.show-date').css({
+        display:'none'
+    });
+
+
+    if($(this).val() == '自定义'){
+        $('#choose-date').modal('show');
+        $('#choose-date input').val('');
+    }
+
+
+});
+//关闭时间弹窗时
+$('#choose-date .btn-default').on('click',function(){
+
+    $('.datatimeblock').val('今天');
+
+});
+$('#choose-date .close').on('click',function(){
+
+    $('.datatimeblock').val('今天');
+
+});
+//选定时间后
+$('#choose-date .btn-primary').on('click',function(){
+
+    if(!checkedNull('#choose-date')){
+        return false;
+    }
+    var txt1 = $('#choose-date .add-input').eq(0).val();
+    var txt2 = $('#choose-date .add-input').eq(1).val();
+
+    var nowDate = getNewDate();
+
+    if(CompareDate(txt2,nowDate) == true){
+        myAlter('结束日期不能大于当前日期');
+        getFocus1( $(this).parents('.modal-header').find('.add-input').eq(1));
+
+        return false;
+    };
+
+
+    if(CompareDate(txt2,txt1) == false){
+        myAlter('结束日期必须大于开始日期');
+        getFocus1( $(this).parents('.modal-header').find('.add-input').eq(1));
+
+        return false;
+    };
+
+    var date = txt1 + '——' + txt2;
+
+    console.log(date);
+
+    $('.show-date').css({
+        display:'inline-block'
+    });
+
+    $('.show-date').val(date);
+
+    $('#choose-date').modal('hide');
+
+});
+
+$('.show-date').on('focus',function(){
 
     $('#choose-date').modal('show');
+    $('#choose-date input').val('');
 });
